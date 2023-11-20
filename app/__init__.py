9a757eb8e98275b5155194de437eb1ffda86c01c
MatheusCervieri <mathcervieri@gmail.com>
2023-11-20 08:29:33 -0300

    I remove the unecessary coments

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,13 @@ import colorlog
 db = SQLAlchemy()
 
 def configure_logger():
-    # Configurar o logger com colorlog
     handler = colorlog.StreamHandler()
     formatter = colorlog.ColoredFormatter(
         "%(log_color)s%(levelname)-8s%(reset)s %(white)s%(message)s",
-        log_colors={'ERROR': 'red'},  # Defina a cor para mensagens de erro
+        log_colors={'ERROR': 'red'},  
     )
     handler.setFormatter(formatter)
 
-    # Configurar o logger
     logger = logging.getLogger(__name__)
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
@@ -29,24 +27,20 @@ def create_app():
     app = Flask(__name__)
     CORS(app, supports_credentials=True, methods=["GET", "POST", "PUT", "DELETE"])
     
-    # Configurações do aplicativo
     app.config.from_object('config.config')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dev.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 
-    # Inicializações
     db.init_app(app)
 
 
-    # Registra blueprints
     from .views.pessoa_view import pessoa_bp
     app.register_blueprint(pessoa_bp)
     from app.views.teste_view import teste_bp
     app.register_blueprint(teste_bp)
 
-    # Cria as tabelas
     with app.app_context():
         db.create_all()
 
